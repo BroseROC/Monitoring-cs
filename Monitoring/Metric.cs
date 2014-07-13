97025463e06d25579c71a389a395bf770312cffa
Bryan Rose <scuba7183@gmail.com>
2014-07-12 19:14:16 -0600

    Initial code commit

---
+++ Monitoring/Metric.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monitoring
+{
+    public class Metric
+    {
+        public Metric()
+        {
+            this.RunningTotals = new List<RunningTotal>();
+        }
+
+        public List<RunningTotal> RunningTotals;
+    }
+}
