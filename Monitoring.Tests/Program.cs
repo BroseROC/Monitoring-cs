25bb450eade104c069d98fc029110d7d3e4b3e16
Bryan Rose <scuba7183@gmail.com>
2014-09-15 19:06:46 -0600

    Changed projects around, added daemon project (no content yet)

--- Monitoring.Tests/Program.cs
+++ Monitoring.Tests/Program.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Monitoring.RunningTotals;
+using Monitoring.RunningTotals.Defaults;
 
-using Monitoring;
-
-
-namespace Monitoring.Tests
+namespace Monitoring
 {
 	public class Program
 	{
 		static void Main(string[] args)
 		{
+			Metric metric1 = new Metric("metric1");
+			metric1.AddRunningTotal("Average", new RunningTotalDefault_Average(10));
+			metric1.AddRunningTotal("Max", new RunningTotalDefault_Max());
+			metric1.AddRunningTotal("Min", new RunningTotalDefault_Min());
 
+			for (decimal i = -24; i < 24; i++)
+			{
+				metric1.ApplyValue(i);
+			}
+
+
+			var values = metric1.GetCurrentValues();
+			foreach (var items in values)
+			{
+				Console.WriteLine("{0}: {1}", items.Name, items.Value);
+			}
+
+			Console.WriteLine("Done.");
+			Console.ReadLine();
 		}
 	}
 }
--- Monitoring/Program.cs
+++ Monitoring/Program.cs
@@ -13,25 +13,7 @@ namespace Monitoring
 	{
 		static void Main(string[] args)
 		{
-			Metric metric1 = new Metric("metric1");
-			metric1.AddRunningTotal("Average", new RunningTotalDefault_Average(10));
-			metric1.AddRunningTotal("Max", new RunningTotalDefault_Max());
-			metric1.AddRunningTotal("Min", new RunningTotalDefault_Min());
 
-			for (decimal i = -24; i < 24; i++)
-			{
-				metric1.ApplyValue(i);
-			}
-				
-
-			var values = metric1.GetCurrentValues();
-			foreach (var items in values)
-			{
-				Console.WriteLine("{0}: {1}", items.Name, items.Value);
-			}
-
-			Console.WriteLine("Done.");
-			Console.ReadLine();
 		}
 	}
 }
